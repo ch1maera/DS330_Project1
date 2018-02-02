e31bc3ed9a37d644a7a954a0cd25c89c83bb2056
Chimaera <31554013+ch1maera@users.noreply.github.com>
2018-02-01 23:02:53 -0600

    Some more stuff

--- reducer3.py
+++ reducer3.py
@@ -1,24 +1,16 @@
 from itertools import chain, combinations
 
-# def all_subsets(ss):
-#   return chain(*map(lambda x: combinations(ss, x), range(0, len(ss)+1)))
-#
-# stuff = '123456'
-#
-# for subset in all_subsets(stuff):
-#     if len(subset) > 1 and subset[0] == '1':
-#         output = ''
-#         for i in subset:
-#             output += str(i + ' ')
-#         output += ': '
-#         print(output)
+def all_subsets(ss):
+  return chain(*map(lambda x: combinations(ss, x), range(2, len(ss)+1)))
+
+
 
 product_dict = {}
+current_key = None
 
 
-with open('reducer3test.txt','r') as e:
+with open('intersorted.txt','r') as e:
     line = e.readline()
-    current_key = line.split(':')[0].strip()
 
     while line:
         if len(line.split(':')) == 2 and '!' not in line:
@@ -26,18 +18,64 @@ with open('reducer3test.txt','r') as e:
             product = line.split(':')[0].strip()
             match = line.split(':')[-1].strip()
 
+            if not current_key:
+                current_key = key
+
         if line.count(':') == 2:
             key = line.split(':')[0].strip()
             product = line.split(':')[1].strip()
             match = line.split(':')[-1].strip()
+
+            if not current_key:
+                current_key = key
 
         if '!' in line:
             key = line.split('!')[0].strip()
             product = line.split('!')[-1].split(':')[0].strip()
             match = line.split('!')[-1].split(':')[-1].strip()
 
+            if not current_key:
+                current_key = key
+
         if current_key != key:
-            print(product_dict)
+            output = ''
+            list_of_keys = []
+            list_of_keys = list(product_dict.keys())
+            list_of_keys.sort()
+            for prod in list_of_keys:
+                if current_key <= prod:
+                    output += prod
+
+
+            for subset in all_subsets(output):
+                keys_to_search = []
+                items_to_append = []
+                if len(subset) > 1 and subset[0] == current_key:
+                    output = ''
+                    for i in subset:
+                        output += str(i + ' ')
+                        if i != current_key:
+                            keys_to_search.append(i)
+                    output += ': '
+                    for matching_product in product_dict[current_key]:
+                        should_append = False
+
+                        for other_product in keys_to_search:
+                            if matching_product not in product_dict[other_product]:
+                                break
+                            else:
+                                should_append = True
+
+                        if should_append == True:
+                            if matching_product not in items_to_append:
+                                if matching_product not in output:
+                                    items_to_append.append(matching_product)
+
+                    for item in items_to_append:
+                        output += item + ' '
+
+                    print(output.strip())
+
             product_dict = {}
             current_key = key
 
@@ -49,6 +87,3 @@ with open('reducer3test.txt','r') as e:
             product_dict[product] = list(match)
 
         line = e.readline()
-
-
-    print(product_dict)
